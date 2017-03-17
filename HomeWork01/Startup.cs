8a7fa554427a09556eb0613c5f3ebefea092b9df
Pc00 <Pc00@X550JX>
2017-03-17 20:36:43 +0800

    加入專案檔案。

---
+++ HomeWork01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeWork01.Startup))]
+namespace HomeWork01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
